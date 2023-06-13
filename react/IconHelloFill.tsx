4102c1c60336f81870151ca582b549135866d839
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2023-06-13 09:07:13 +0000

    [Icona] Generate React Components

---
+++ react/IconHelloFill.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const IconHelloFill = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" {...props}>
+    <path
+      fill="currentColor"
+      stroke="currentColor"
+      strokeWidth={1.6}
+      d="M6.8 4A1.2 1.2 0 0 1 8 2.8h9A1.2 1.2 0 0 1 18.2 4v13.2H6.8V4Zm4.075-.6a1.6 1.6 0 1 0 0 3.2h3.25a1.6 1.6 0 1 0 0-3.2h-3.25ZM6.8 19.8h11.4v.2a1.2 1.2 0 0 1-1.2 1.2H8A1.2 1.2 0 0 1 6.8 20v-.2Z"
+    />
+  </svg>
+);
+export default IconHelloFill;
---
+++ react/IconHelloRegular.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const IconHelloRegular = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" {...props}>
+    <g fill="currentColor">
+      <path d="M10.875 4.2a.8.8 0 0 0 0 1.6h3.25a.8.8 0 0 0 0-1.6h-3.25Z" />
+      <path
+        fillRule="evenodd"
+        d="M6 4v16a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2Zm11-.4H8a.4.4 0 0 0-.4.4v15h9.8V4a.4.4 0 0 0-.4-.4Z"
+        clipRule="evenodd"
+      />
+    </g>
+  </svg>
+);
+export default IconHelloRegular;
