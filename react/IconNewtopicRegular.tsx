97dcad7b82a92dd36c6635de11e1d2daa1add7cc
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2023-07-20 03:50:16 +0000

    [Icona] Generate Files

--- react/IconNewtopicRegular.tsx
+++ react/IconNewtopicRegular.tsx
@@ -6,7 +6,7 @@ const IconNewtopicRegular = (props: SVGProps<SVGSVGElement>) => (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     data-seed-icon="true"
-    data-seed-icon-version="0.3.3"
+    data-seed-icon-version="0.3.5"
     {...props}
   >
     <g>
