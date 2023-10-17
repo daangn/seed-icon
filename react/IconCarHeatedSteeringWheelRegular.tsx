2c594070eb8fa67670ad15e063678bc42abe743e
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2023-10-17 12:16:49 +0900

    [Icona]: Update Icons (#47)
    * feat: update icons.json
    
    * 0.3.11
    
    * [Icona] Generate Files
    
    ---------
    
    Co-authored-by: GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>

--- react/IconCarHeatedSteeringWheelRegular.tsx
+++ react/IconCarHeatedSteeringWheelRegular.tsx
@@ -6,7 +6,7 @@ const IconCarHeatedSteeringWheelRegular = (props: SVGProps<SVGSVGElement>) => (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     data-seed-icon="true"
-    data-seed-icon-version="0.3.8"
+    data-seed-icon-version="0.3.11"
     {...props}
   >
     <g>
